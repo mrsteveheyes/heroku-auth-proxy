d320b293aa475e4e5eb23f09ce68f3088afb1ebc
Steve Heyes <steve@substrakt.com>
2017-09-04 13:14:42 +0100

    ADD: Improve the test descriptions

--- __tests__/server.js
+++ __tests__/server.js
@@ -1,13 +1,15 @@
 const request = require('supertest')
 const app = require('../app')
 
-describe('Should return with an error if no parameters given', () => {
-  it('error test', () => {
-    return request(app).get('/')
-      .expect(400)
-      .then((res) => {
-        expect(res.body.error).toBe('InvalidParameters')
-        expect(res.body.message).toBe("The paramaters you passed where invalid")
-      })
+describe('Heroku Auth Proxy API', () => {
+  describe('GET "/"', () => {
+    it('should return with an error if no parameters given', () => {
+      return request(app).get('/')
+        .expect(400)
+        .then((res) => {
+          expect(res.body.error).toBe('InvalidParameters')
+          expect(res.body.message).toBe("The paramaters you passed where invalid")
+        })
+    })
   })
 })
